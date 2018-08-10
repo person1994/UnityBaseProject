2521b12b07631f4e47efa5c49f2fe37afaae15bb
person1994 <42255170+person1994@users.noreply.github.com>
2018-08-10 03:09:18 +0100

    Created editor script
    Created a new custum editor for the terrain generotor.

---
+++ Assets/Editor/TerrainGeneratorEditor.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(TerrainGenerator))]
+public class TerrainGeneratorEditor : Editor {
+
+    public override void OnInspectorGUI()
+    {
+        TerrainGenerator terrainGenerator = target as TerrainGenerator;
+
+        if(DrawDefaultInspector())
+        {
+            terrainGenerator.GenerateTerrain();
+        }
+
+        if(GUILayout.Button("Generate"))
+        {
+            terrainGenerator.GenerateTerrain();
+        }
+    }
+}
